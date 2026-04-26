22999ccbd03cf90680503d68d4335ab8b976d8dc
mzenner1 <mzenner@firstscribe.com>
2026-04-26 10:14:59 -0500

    fix pwa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link rel="icon" type="image/png" href="{{ asset('images/site-logo-2.png') }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700&display=swap" rel="stylesheet"/>
-    (['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @laravelPWA
     <script src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js" defer></script>
 </head>
