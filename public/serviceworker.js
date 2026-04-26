a8d035fb4891aeb0cda36fabbb01a3c083daf06a
mzenner1 <mzenner@firstscribe.com>
2026-04-26 10:33:54 -0500

    fix service worker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,19 +1,11 @@
 var staticCacheName = "pwa-v" + new Date().getTime();
 var filesToCache = [
-    '/offline',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
-    '/images/icons/icon-192x192.png',
-    '/images/icons/icon-384x384.png',
-    '/images/icons/icon-512x512.png',
+    '/offline'
 ];
 
 // Cache on install
 self.addEventListener("install", event => {
-    this.skipWaiting();
+    self.skipWaiting();
     event.waitUntil(
         caches.open(staticCacheName)
             .then(cache => {
@@ -22,21 +14,21 @@ self.addEventListener("install", event => {
     )
 });
 
-// Clear cache on activate
+// Clear old caches on activate
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames
-                    .filter(cacheName => (cacheName.startsWith("pwa-")))
-                    .filter(cacheName => (cacheName !== staticCacheName))
+                    .filter(cacheName => cacheName.startsWith("pwa-"))
+                    .filter(cacheName => cacheName !== staticCacheName)
                     .map(cacheName => caches.delete(cacheName))
             );
         })
     );
 });
 
-// Serve from Cache
+// Serve from cache, fall back to network, fall back to offline page
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
