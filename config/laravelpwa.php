29e765fd610f7edb6ea05f5c81a07920d34a0ba2
mzenner1 <mzenner@firstscribe.com>
2026-04-26 10:38:41 -0500

    fix pwa

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -3,29 +3,52 @@
 return [
     'name' => 'Zenner Tasks',
     'manifest' => [
-        'name'             => env('APP_NAME', 'Zenner Tasks'),
-        'short_name'       => 'Zenner',
-        'start_url'        => '/',
+        'name' => 'Zenner Tasks',
+        'short_name' => 'Z Tasks',
+        'start_url' => '/dashboard',
         'background_color' => '#000000',
-        'theme_color'      => '#000000',
-        'display'          => 'standalone',
-        'orientation'      => 'any',
-        'status_bar'       => 'black',
+        'theme_color' => '#000000',
+        'display' => 'standalone',
+        'orientation'=> 'any',
+        'status_bar'=> 'black',
         'icons' => [
-            '72x72'  => ['path' => '/images/icons/icon-72x72.png',  'purpose' => 'any'],
-            '96x96'  => ['path' => '/images/icons/icon-96x96.png',  'purpose' => 'any'],
-            '128x128'=> ['path' => '/images/icons/icon-128x128.png','purpose' => 'any'],
-            '144x144'=> ['path' => '/images/icons/icon-144x144.png','purpose' => 'any'],
-            '152x152'=> ['path' => '/images/icons/icon-152x152.png','purpose' => 'any'],
-            '192x192'=> ['path' => '/images/icons/icon-192x192.png','purpose' => 'any'],
-            '384x384'=> ['path' => '/images/icons/icon-384x384.png','purpose' => 'any'],
-            '512x512'=> ['path' => '/images/icons/icon-512x512.png','purpose' => 'any'],
-            '512x512-maskable' => ['path' => '/images/icons/icon-maskable-512x512.png', 'purpose' => 'maskable'],
+            '72x72' => [
+                'path' => '/images/icons/icon-72x72.png',
+                'purpose' => 'any'
+            ],
+            '96x96' => [
+                'path' => '/images/icons/icon-96x96.png',
+                'purpose' => 'any'
+            ],
+            '128x128' => [
+                'path' => '/images/icons/icon-128x128.png',
+                'purpose' => 'any'
+            ],
+            '144x144' => [
+                'path' => '/images/icons/icon-144x144.png',
+                'purpose' => 'any'
+            ],
+            '152x152' => [
+                'path' => '/images/icons/icon-152x152.png',
+                'purpose' => 'any'
+            ],
+            '192x192' => [
+                'path' => '/images/icons/icon-background.png',
+                'purpose' => 'maskable'
+            ],
+            '384x384' => [
+                'path' => '/images/icons/icon-384x384.png',
+                'purpose' => 'any'
+            ],
+            '512x512' => [
+                'path' => '/images/icons/icon-512x512.png',
+                'purpose' => 'any'
+            ],
         ],
         'splash' => [
-            '640x1136'  => '/images/icons/splash-640x1136.png',
-            '750x1334'  => '/images/icons/splash-750x1334.png',
-            '828x1792'  => '/images/icons/splash-828x1792.png',
+            '640x1136' => '/images/icons/splash-640x1136.png',
+            '750x1334' => '/images/icons/splash-750x1334.png',
+            '828x1792' => '/images/icons/splash-828x1792.png',
             '1125x2436' => '/images/icons/splash-1125x2436.png',
             '1242x2208' => '/images/icons/splash-1242x2208.png',
             '1242x2688' => '/images/icons/splash-1242x2688.png',
@@ -36,18 +59,28 @@ return [
         ],
         'shortcuts' => [
             [
-                'name'        => 'Dashboard',
+                'name' => 'Dashboard',
                 'description' => 'Go to your dashboard',
-                'url'         => '/dashboard',
-                'icons'       => ['src' => '/images/icons/icon-96x96.png', 'purpose' => 'any'],
+                'url' => '/dashboard',
+                'icons' => [
+                    'src' => '/images/icons/icon-72x72.png',
+                    'purpose' => 'any'
+                ]
             ],
             [
-                'name'        => 'Projects',
+                'name' => 'Projects',
                 'description' => 'View all projects',
-                'url'         => '/projects',
-                'icons'       => ['src' => '/images/icons/icon-96x96.png', 'purpose' => 'any'],
-            ],
+                'url' => '/projects',
+                'icons' => [
+                    'src' => '/images/icons/icon-72x72.png',
+                    'purpose' => 'any'
+                ]
+            ]
         ],
-        'custom' => []
+        'custom' => [],
+        'shortcuts_items' => [
+            'create' => false
+        ],
+        'badge' => '/images/icons/s-mono-256.png'
     ]
 ];
